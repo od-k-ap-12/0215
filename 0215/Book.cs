abe42b4c7ce93f010e040aa546e845e86fcf8041
Oleksandra <predictormagnolia@gmail.com>
2023-02-20 01:55:31 +0200

    Add project files.

---
+++ 0215/Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0215
+{
+    class Book:ICloneable,IComparable,IComparer 
+    {
+        public string Author;
+        public string Name;
+        public int PublishingYear;
+        public string Genre;
+        public Book(string _Author, string _Name, int _PublishingYear, string _Genre)
+        {
+            Author = _Author;
+            Name = _Name;
+            PublishingYear = _PublishingYear;
+            Genre = _Genre;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Author: " + Author + "\nName:" + Name + "\nPublished:" + PublishingYear + "\nGenre:" + Genre);
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter author: ");
+            Author = Console.ReadLine();
+            Console.WriteLine("Enter name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter publishing year: ");
+            PublishingYear =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter genre: ");
+            Genre = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return "Author: " + Author + "\nName:" + Name + "\nPublished:" + PublishingYear + "\nGenre:" + Genre;
+        }
+
+        public object Clone()
+        {
+            return new Book(Author, Name, PublishingYear, Genre);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Book)
+            {
+                return this.PublishingYear.CompareTo((obj as Book).PublishingYear);
+            }
+            throw new NotImplementedException();
+        }
+
+        public int Compare(object x, object y)
+        {
+            if(x is Book && y is Book)
+            {
+                return (x as Book).PublishingYear.CompareTo((y as Book).PublishingYear);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ 0215/Library.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0215
+{
+    class Library:IEnumerable,IEnumerator
+    {
+        int curpos = -1;
+        private Book[] arr;
+        public Library(params Book[] book)
+        {
+            arr = new Book[book.Length];
+            for (int i = 0; i < book.Length; i++)
+                arr[i] = book[i];
+        }
+        public Library(int size)
+        {
+            arr = new Book[size];
+        }
+        public Book this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arr.Length)
+                {
+                    return arr[index];
+                }
+                else
+                {
+                    throw new Exception("Invalid index");
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < arr.Length)
+                {
+                    arr[index] = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid index");
+                }
+            }
+
+        }
+
+        public object Current
+        {
+            get
+            {
+                return arr[curpos];
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if ((int)Current < arr.Length - 1)
+            {
+                curpos++;
+                return true;
+            }
+            else
+            {
+                this.Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+           curpos = -1;
+        }
+    }
+}
